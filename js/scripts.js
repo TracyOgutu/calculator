6bc2296dfe932496948c29c094fdb33521df1414
Tracy Ogutu <tracyogutu@gmail.com>
2019-09-19 14:29:05 +0300

    added a division and multiplication function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,38 @@
-var add = function(number1, number2) {
-    return number1 + number2;
-  };
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
+
+
+  function add() {
+    alert("We're adding numbers")
+    var number1 = parseInt(prompt("Enter first number:"));
+    var number2 = parseInt(prompt("Enter second number:"));
+    
+    alert("The addition result of:" +" " + number1+ " "+ "and"+" "+
+    number2 + " "+"is:"+" "+ parseInt(number1 + number2));
   
-  alert(add(number1, number2));
+    }
+    add();
+
+
+    
+  function multiply(){
+    alert("We're multiplying numbers")
+    var num1=parseInt(prompt("Enter first number"));
+    var num2=parseInt(prompt("Enter second number"));
+
+    alert("The result of your multiplication is :" + num1*num2);
+
+  }
+  multiply();
+
+
+  
+  function division(){
+    alert("We're dividing numbers");
+    var number1 = parseInt(prompt("Enter first number"));
+    var number2 = parseInt(prompt("Enter second number"));
+
+    alert("The result of your division is :"+ number1/number2);
+  }
+  division();
+
+
+
